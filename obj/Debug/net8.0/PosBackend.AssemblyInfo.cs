d1ca8d00ce604c77852897d63f9d822ffe56a188
Pinto Manuel <pintoydc@gmail.com>
2024-10-14 20:34:37 +0200

    updated comment

--- obj/Debug/net8.0/PosBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/PosBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a865c201441deee7517c174c7be979f0689cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
