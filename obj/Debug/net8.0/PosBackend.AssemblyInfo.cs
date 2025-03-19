b04479024f818dd76dd52a0765effb38b73fd555
Pinto Manuel <pintoydc@gmail.com>
2025-03-19 08:18:22 +0200

    commit code

--- obj/Debug/net8.0/PosBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/PosBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cef618616495887aeab84a1db094a987bef4d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847855963d464d29a5c85cdc598dfdb910da049c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PosBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
